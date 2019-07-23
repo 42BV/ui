13dfa6c7bb0e4fed5349433ecd6568530532490c
jvhoven <contact@jvhoven.nl>
2019-07-23 14:49:33 +0200

    Add MoreOrLess

--- packages/core/bundle/src/index.ts
+++ packages/core/bundle/src/index.ts
@@ -4,3 +4,4 @@ export { default as Avatar } from '@42.nl/ui-core-avatar';
 export { default as InfoBadge } from '@42.nl/ui-core-infobadge';
 export { default as FlashMessage } from '@42.nl/ui-core-flash-message';
 export { default as Spinner } from '@42.nl/ui-core-spinner';
+export { default as MoreOrLess } from '@42.nl/ui-core-more-or-less';
