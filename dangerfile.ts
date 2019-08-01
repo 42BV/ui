d9cf899c45b296cf41814fcf428268d40ebbd32c
jvhoven <contact@jvhoven.nl>
2019-08-01 16:31:01 +0200

    chore: drop danger-plugin-console

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,7 @@
 import jest from 'danger-plugin-jest';
-import noConsole from 'danger-plugin-no-console';
 import yarn from 'danger-plugin-yarn';
 
 export default async () => {
-  await noConsole({ whitelist: ['error'] });
   await yarn({ showSuccessMessage: true });
   await jest();
 };
