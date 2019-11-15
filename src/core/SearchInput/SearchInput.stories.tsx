af8adf6162784acabbbbab199531cdf80d4e00bb
Maarten Hus <maarten.hus@42.nl>
2019-11-15 15:06:13 +0100

    improvement: SearchInput's value can now be manipulated from the outside.
    The `SearchInput` is debounced and therefore must use an uncontrolled
    input. Otherwise the value of the input cannot be debounced.
    
    This causes a problem however, when ever the `value` prop of the
    `SearchInput` changed due to external changes. For example: a button
    which clears the `SearchInput`. The new value would not appear in the
    input element, due to it being uncontrolled.
    
    This commit adds a new children prop to the `SearchInput`. The children
    is expected to be a function of type:
    
    ```ts
    (searchInput: React.ReactNode, api: SearchInputApi) => React.ReactNode;
    ``
    
    It should take the searchInput rendered by the `SearchInput` and display
    it somewhere in the return of the children function. It can the use the
    `api` which contains a function called `setValue` to change the actual
    value of the `SearchInput`.
    
    The `setValue` will cancel the debounce, update the `<input>`'s actual
    value through the ref, and it will call `props.onChange` with the actual
    value.
    
    Closes: #219

--- src/core/SearchInput/SearchInput.stories.tsx
+++ src/core/SearchInput/SearchInput.stories.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import SearchInput from './SearchInput';
+
 import { Card } from 'reactstrap';
+import { Select, Button } from '../..';
 
 storiesOf('core|SearchInput', module)
   .addParameters({ component: SearchInput })
@@ -48,6 +50,42 @@ storiesOf('core|SearchInput', module)
           debounce={1000}
           debounceSettings={{ leading: true, trailing: true }}
         />
+      </Card>
+    );
+  })
+  .add('with external value', () => {
+    const [query, setQuery] = useState('');
+
+    return (
+      <Card body>
+        <p>You searched for: {query}</p>
+
+        <SearchInput value={query} onChange={setQuery} debounce={1000}>
+          {(searchInput, { setValue }) => (
+            <>
+              {searchInput}
+
+              <Button className="mt-3" onClick={() => setValue('')}>
+                Clear query
+              </Button>
+
+              <Select
+                className="mt-2"
+                id="predefined-query"
+                label="Predefined queries"
+                value={query}
+                placeholder="Please select a predefined query"
+                optionForValue={option => option}
+                options={['Maarten', 'Jeffrey']}
+                onChange={value => {
+                  if (value) {
+                    setValue(value);
+                  }
+                }}
+              />
+            </>
+          )}
+        </SearchInput>
       </Card>
     );
   });
