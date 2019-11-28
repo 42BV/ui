42BV/ui

commit e9e2693bb944904b116d5dcca515ec8975e0cbb5

bug: `EpicTable` no longer flashes when moving to the flat mode.
When the `EpicTable` switched flat and normal the shadows shown by normal
mode would show very briefly.

The reason for this is because the `EpicTable` calculates in which mode
it needs to render by checking if the desired width requirements have
been met. It can only do this by actually rendering itself. Initially
it will assume the `normal` render, if everything fits it will render
in `flat` mode. This switching between them causes a render flash.

The solution is setting the CSS property `visibility: hidden;` on the
`EpicTable` and on the `GooeyCenter`'s center div. With `visibility: hidden;`
the browser will not show the element but does let it take up the space.
This way we do not shown anything until the calculation has been made.
This of course prevents any flickering.

Fixes: #240

src/table/EpicTable/EpicTable.tsx
@@ -122,7 +122,10 @@ export function EpicTable({
   const desiredWidthMet = totalDesiredCenterWidth < centerWidth;
 
   const classes = classNames('epic-table', {
-    'epic-table--striped': striped
+    'epic-table--striped': striped,
+    // Making it invisible when not fully calculated, prevents
+    // the content from instantly resizing and flickering.
+    invisible: centerWidth === 0
   });
 
   return (

src/table/EpicTable/helpers/GooeyCenter/GooeyCenter.tsx
@@ -111,8 +111,10 @@ export function GooeyCenter({
   const centerWrapper = (
     <div
       ref={centerEl}
-      // Making it undefined when not fully calculated, prevents
+      // Making it invisible when not fully calculated, prevents
       // the content from instantly resizing and flickering.
+      // Also the width must be undefined for this to work.
+      className={centerWidth === 0 ? 'invisible' : ''}
       style={{ width: centerWidth !== 0 ? centerWidth : undefined }}
     >
       {center}

commit db66bac9f4dbd43ff4c73738cb4f1873d217115b

fix: FixedHeader was broken and did not render correctly.
The reason was a regression in `e9e2693bb` which changed the way the
`EpicLayout` was created. Since that commit an empty header was accidentally
always added on the left.

This causes the arrays length for the center, right and left to diverge.
Since the `FixedHeader` looks at the `left` to determine which row
index it should make fixed, when these arrays vary you get null pointers.

To fix this I've done the following:

  1. In the `layout` the left now has the same guards against empty sections
     and rows as the right and center have. This way we prevent empty
     sections and rows from existing.

  2. The `FixedHeader` when shown would show flashes. Ironically these
     flashes were caused by the guards against flashing. The `GooeyCenter`
     is invisible until the `centerWidth` has been calculated. This
     works for all non `FixedHeader` `GooeyCenter`'s', but not for the
     `FixedHeader`.

     So added a boolean prop called `forceCenterAlwaysVisible` on the
     `GooeyCenter`. The `FixedHeader` sets this to `true` and this way
     there is no flash, for the `FixedHeader`.

  3. Added guards in the `FixedHeader` against causing null pointers.
     This way when something goes wrong at least the application can
     still be used, even though the `FixedHeader` will probably look
     strange.

  4. All stories are written now in a way that promotes triggering
     the FixedHeader. Before the content was often to small to trigger
     the FixedHeader, making these bugs harder to catch.

Also removed the shadows in the `FixedHeader` which looked strange.

Made a tiny refactoring in the code which determines if shadows should
be rendered in the `EpicTable`. Now they are defined in `const`'s for
re-use.

Fixes: #247

src/table/EpicTable/EpicTable.stories.tsx
@@ -99,7 +99,7 @@ storiesOf('table|EpicTable', module)
 
     const [page, setPage] = useState(1);
 
-    const pageOfPersons = pageOf(sortedPersons, page);
+    const pageOfPersons = pageOf(sortedPersons, page, 20);
 
     const [selected, setSelected] = useState<Person[]>([]);
 
@@ -144,15 +144,6 @@ storiesOf('table|EpicTable', module)
       }, 1000);
     }, []);
 
-    const overlay = loading ? (
-      <ContentState mode="loading" title="Loading" />
-    ) : pageOfPersons.totalElements === 0 ? (
-      <ContentState
-        mode="no-results"
-        title="Try again with a different query"
-      />
-    ) : null;
-
     return (
       <Card body>
         {selected.length > 0 ? (
@@ -170,7 +161,19 @@ storiesOf('table|EpicTable', module)
           </>
         ) : null}
 
-        <EpicTable overlay={overlay}>
+        <EpicTable
+          overlay={
+            loading ? (
+              <ContentState className="my-5" mode="loading" title="Loading" />
+            ) : pageOfPersons.totalElements === 0 ? (
+              <ContentState
+                className="my-5"
+                mode="no-results"
+                title="Try again with a different query"
+              />
+            ) : null
+          }
+        >
           <EpicRow header>
             <EpicHeader
               width={widths.firstName}
@@ -1293,7 +1296,7 @@ storiesOf('table|EpicTable', module)
   .add('with pagination', () => {
     const [page, setPage] = useState(1);
 
-    const pageOfPersons = pageOf(persons, page);
+    const pageOfPersons = pageOf(persons, page, 20);
 
     return (
       <Card body>
@@ -1542,7 +1545,7 @@ storiesOf('table|EpicTable', module)
             </EpicHeader>
           </EpicRow>
 
-          {persons.slice(0, 4).map((person, index) => (
+          {persons.map((person, index) => (
             <Fragment key={person.id}>
               <EpicRow>
                 <EpicCell width={300} height={44}>
@@ -1673,7 +1676,7 @@ storiesOf('table|EpicTable', module)
   .add('with selection', () => {
     const [page, setPage] = useState(1);
 
-    const pageOfPersons = pageOf(persons, page);
+    const pageOfPersons = pageOf(persons, page, 20);
 
     const [selected, setSelected] = useState<Person[]>([]);
 
@@ -2079,6 +2082,118 @@ const persons: Person[] = [
     favoriteMovie: 'Next generation',
     favoriteFood: 'Tea Earl Grey Hot',
     birthDate: '2200-09-24',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Peter',
+    lastName: 'Parker',
+    age: 30,
+    eyeColor: 'blue',
+    height: 55,
+    weight: 14,
+    jobTitle: 'Spider-man',
+    favoriteMovie: 'Spider-man',
+    favoriteFood: 'Webs',
+    birthDate: '1990-09-24',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Clark',
+    lastName: 'Kent',
+    age: 40,
+    eyeColor: 'blue',
+    height: 80,
+    weight: 33,
+    jobTitle: 'Journalist',
+    favoriteMovie: 'Superman returns',
+    favoriteFood: 'Kryptonite',
+    birthDate: '1960-01-01',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+    age: 55,
+    eyeColor: 'blue',
+    height: 70,
+    weight: 33,
+    jobTitle: 'CEO',
+    favoriteMovie: 'Batman begins',
+    favoriteFood: 'Bats',
+    birthDate: '1955-01-01',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Diana',
+    lastName: 'Prince',
+    age: 28,
+    eyeColor: 'green',
+    height: 90,
+    weight: 19,
+    jobTitle: 'Curator',
+    favoriteMovie: 'Wonderwoman',
+    favoriteFood: 'Greek',
+    birthDate: '1990-01-01',
+    sex: 'female'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Tony',
+    lastName: 'Stark',
+    age: 40,
+    eyeColor: 'brown',
+    height: 70,
+    weight: 33,
+    jobTitle: 'CEO',
+    favoriteMovie: 'Ironman',
+    favoriteFood: 'Shoarma',
+    birthDate: '1980-01-01',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Steve',
+    lastName: 'Rogers',
+    age: 100,
+    eyeColor: 'blue',
+    height: 44,
+    weight: 55,
+    jobTitle: 'Captain',
+    favoriteMovie: 'Winter soldier',
+    favoriteFood: 'Applepie',
+    birthDate: '1920-01-01',
+    sex: 'male'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Natasha',
+    lastName: 'Romanov',
+    age: 30,
+    eyeColor: 'green',
+    height: 77,
+    weight: 66,
+    jobTitle: 'Black widow',
+    favoriteMovie: 'Avengers',
+    favoriteFood: 'Stroganov',
+    birthDate: '1995-01-01',
+    sex: 'female'
+  },
+  {
+    id: Math.random(),
+    firstName: 'Bruce',
+    lastName: 'Banner',
+    age: 42,
+    eyeColor: 'brown',
+    height: 89,
+    weight: 99,
+    jobTitle: 'Smasher',
+    favoriteMovie: 'The Incredible Hulk',
+    favoriteFood: 'Gammarays',
+    birthDate: '1975-01-01',
     sex: 'male'
   }
 ];

src/table/EpicTable/EpicTable.tsx
@@ -128,10 +128,15 @@ export function EpicTable({
     invisible: centerWidth === 0
   });
 
+  const leftAndContainerHaveShadow = !desiredWidthMet && !overlay;
+  const rightHasShadow = leftAndContainerHaveShadow && !containsActiveDetailRow;
+
   return (
     <div ref={epicTableEl} className={classes} style={{ minHeight: minHeight }}>
       <div
-        className={`epic-table-container ${desiredWidthMet ? 'shadow' : ''}`}
+        className={`epic-table-container ${
+          leftAndContainerHaveShadow ? 'shadow' : ''
+        }`}
       >
         {overlay && epicTableRect ? (
           <div
@@ -158,7 +163,7 @@ export function EpicTable({
           onScroll={setLeftScroll}
           left={
             <div
-              className={desiredWidthMet || overlay ? '' : 'shadow'}
+              className={leftAndContainerHaveShadow ? 'shadow' : ''}
               style={{ minHeight }}
             >
               {left.map((section, index) => (
@@ -194,13 +199,7 @@ export function EpicTable({
           }
           right={
             hasRight && right && right.length > 0 ? (
-              <div
-                className={
-                  desiredWidthMet || containsActiveDetailRow || overlay
-                    ? ''
-                    : 'shadow'
-                }
-              >
+              <div className={rightHasShadow ? 'shadow' : ''}>
                 {right &&
                   right.map((section, index) => (
                     <Fragment key={index}>

src/table/EpicTable/helpers/FixedHeader/FixedHeader.tsx
@@ -67,9 +67,10 @@ export function FixedHeader({
     <div className="epic-table-fixed-header" ref={fixedHeaderEl}>
       {showFixedHeader ? (
         <GooeyCenter
-          left={<div className="shadow">{left[index].header}</div>}
+          forceCenterAlwaysVisible={true}
+          left={left && left[index] ? left[index].header : null}
           center={
-            center && center.length > 0 ? (
+            center && center[index] ? (
               <div
                 ref={fixedCenterEl}
                 className="d-flex justify-content-between overflow-hidden"
@@ -79,11 +80,7 @@ export function FixedHeader({
               </div>
             ) : null
           }
-          right={
-            right && right.length > 0 ? (
-              <div className="shadow">{right[index].header}</div>
-            ) : null
-          }
+          right={right && right[index] ? right[index].header : null}
           showScrollbar={false}
         />
       ) : null}

src/table/EpicTable/helpers/GooeyCenter/GooeyCenter.tsx
@@ -43,6 +43,12 @@ type Props = {
    * sync the center width.
    */
   onCenterWidthChanged?: (centerWidth: number) => void;
+
+  /**
+   * Whether or not the center is always visible. Need for the
+   * FixedHeader to prevent it from flashing.
+   */
+  forceCenterAlwaysVisible?: boolean;
 };
 
 /**
@@ -63,7 +69,8 @@ export function GooeyCenter({
   center,
   onScroll,
   showScrollbar = true,
-  onCenterWidthChanged
+  onCenterWidthChanged,
+  forceCenterAlwaysVisible = false
 }: Props) {
   const wrapperEl = useRef<HTMLDivElement>(null);
   const centerEl = useRef<HTMLDivElement>(null);
@@ -114,7 +121,9 @@ export function GooeyCenter({
       // Making it invisible when not fully calculated, prevents
       // the content from instantly resizing and flickering.
       // Also the width must be undefined for this to work.
-      className={centerWidth === 0 ? 'invisible' : ''}
+      className={
+        centerWidth === 0 && !forceCenterAlwaysVisible ? 'invisible' : ''
+      }
       style={{ width: centerWidth !== 0 ? centerWidth : undefined }}
     >
       {center}

src/table/EpicTable/helpers/layout/layout.ts
@@ -264,21 +264,17 @@ export function epicTableLayout(
     const isHeader = row.props.header;
 
     // When new header starts end the section and start a new one.
-    if (isHeader && index !== 0) {
+    if (isHeader) {
       if (leftSection !== null) {
-        left.push(leftSection);
+        if (sectionHasData(leftSection)) {
+          left.push(leftSection);
+        }
 
-        if (
-          centerSection.header.length > 0 ||
-          centerSection.contents.length > 0
-        ) {
+        if (sectionHasData(centerSection)) {
           center.push(centerSection);
         }
 
-        if (
-          rightSection.header.length > 0 ||
-          rightSection.contents.length > 0
-        ) {
+        if (sectionHasData(rightSection)) {
           right.push(rightSection);
         }
       }
@@ -331,7 +327,9 @@ export function epicTableLayout(
       }
     });
 
-    leftSection.contents.push(leftRow);
+    if (leftRow.length > 0) {
+      leftSection.contents.push(leftRow);
+    }
 
     if (centerRow.length > 0) {
       // Only calculate it once for the first center row encountered.
@@ -420,4 +418,8 @@ export function getRows(children: any): ReactElement[] {
   });
 
   return rows;
+}
+
+function sectionHasData(section: EpicTableLayoutSection): boolean {
+  return section.contents.length > 0 || section.header.length > 0;
 }