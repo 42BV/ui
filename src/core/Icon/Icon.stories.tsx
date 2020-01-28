1554c0c1a5fbe648ce5f1cfc002c56a0db700b43
Maarten Hus <maarten.hus@42.nl>
2020-01-28 11:07:21 +0100

    improvement: `Icon` component now supports optional font size property.
    Closes: #274

--- src/core/Icon/Icon.stories.tsx
+++ src/core/Icon/Icon.stories.tsx
@@ -21,20 +21,18 @@ storiesOf('core|Icons', module)
   })
   .add('examples', () => {
     return (
-      <div className="text-center">
+      <div className="d-flex flex-column align-items-center">
         <Icon icon="child_care" color="danger" />
         <Icon icon="adb" color="info" />
         <Icon icon="restaurant" color="muted" />
         <Icon icon="train" color="primary" />
         <Icon icon="wb_sunny" color="warning" />
+        <Icon icon="info" size={144} />
+        <Icon icon="info" size={20} />
         <Icon icon="drafts" />
         <Icon icon="drafts" disabled={true} />
         <Icon icon="timer_3" onClick={action('timer clicked')} />
-        <Icon
-          icon="home"
-          disabled={true}
-          onClick={action('timer clicked')}
-        />
+        <Icon icon="home" disabled={true} onClick={action('timer clicked')} />
       </div>
     );
   });
