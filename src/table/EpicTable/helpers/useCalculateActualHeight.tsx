2a1a1ccd58276c986a6dee2c4797ae5c1ce0cb38
Maarten Hus <maarten.hus@42.nl>
2020-02-10 09:38:51 +0100

    fix: EpicTable did not resize when EpicDetail / overlay became bigger.
    This happens when the `EpicDetail` displays content it loads
    asynchronously from the back-end. After the content is loaded the
    `EpicDetail` can becomes larger than the loading state quite easily.
    This could also happen with an overlay as well.
    
    This results in the `EpicDetail` rendering over the `EpicTable`'s
    bounds.
    
    With this fix the `useCalculateActualHeight` now takes into account the
    size of the overlay and the EpicDetail.
    
    For the `EpicDetail` an interval is introduced which at every 20
    milliseconds checks if the `EpicDetail` has become bigger. This way we
    spot changes in size quickly.
    
    Unfortunately it is not possible to make the `EpicDetail` shrink when
    it becomes smaller. This is because we set the `style.height` of the
    detail manually which makes the `scrollHeight` unusable because we
    override it. But this at least does not look as bad as when the
    `EpicDetail` is rendered outside of the `EpicTable`.
    
    Fixes #298.

---
+++ src/table/EpicTable/helpers/useCalculateActualHeight.tsx
@@ -0,0 +1,80 @@
+/* istanbul ignore file */
+
+// The EpicTable is really large and difficult to test via unit tests.
+// Therefore there are a ton of stories for e2e testing instead. So
+// that is why the EpicTable is ignored by istanbul.
+
+import { useState, useEffect } from 'react';
+
+type CalculateActualHeightConfig = {
+  minHeight: number;
+  totalDesiredHeight: number;
+  activeDetailRow: HTMLDivElement | null;
+  overlayEl: HTMLDivElement | null;
+  headerHeight: number;
+};
+
+/**
+ * Calculates the height of the EpicTable. Taking into account
+ * the existence of an active EpicDetailRow or overlay.
+ *
+ * When an EpicDetailRow exists it may be larger than the actual
+ * EpicTable. So we increase the height of the EpicTable in that
+ * case to be the height of the EpicDetailRow.
+ *
+ * Also the minHeight can be lower than the totalDesiredHeight. In
+ * that case the totalDesiredHeight should be used instead. As the
+ * minHeight is merely a suggestion.
+ */
+export function useCalculateActualHeight({
+  minHeight,
+  totalDesiredHeight,
+  activeDetailRow,
+  overlayEl,
+  headerHeight
+}: CalculateActualHeightConfig) {
+  const [actualHeight, setActualHeight] = useState(minHeight);
+  useEffect(() => {
+    function resize() {
+      const detailRowHeight = activeDetailRow
+        ? activeDetailRow.scrollHeight
+        : 0;
+
+      const overlayHeight = overlayEl
+        ? overlayEl.scrollHeight + headerHeight
+        : 0;
+
+      const height = Math.max(
+        minHeight,
+        detailRowHeight,
+        totalDesiredHeight,
+        overlayHeight
+      );
+      setActualHeight(height);
+    }
+
+    /*
+      When there is an active DetailRow then resize the content
+      after every 20 milliseconds. This needs to be done so that
+      when the DetailRow becomes larger after it has been rendered
+      the EpicTable scales with it.
+ 
+      Unfortunately when the detail row becomes smaller later on we
+      lose the ability to shrink the detail row. This is because when
+      we set the `style.height` of the detail row we cannot access
+      the needed size again via the `scrollHeight`.
+    */
+    let interval = -1;
+    if (activeDetailRow) {
+      interval = window.setInterval(resize, 20);
+    }
+
+    resize();
+
+    return () => {
+      window.clearInterval(interval);
+    };
+  }, [minHeight, totalDesiredHeight, activeDetailRow, overlayEl, headerHeight]);
+
+  return actualHeight;
+}
