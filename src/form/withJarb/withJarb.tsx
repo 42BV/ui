0cfe4b06ad779314e63022b8caa6d3d7828ecd37
Maarten Hus <maarten.hus@42.nl>
2020-03-13 12:36:43 +0100

    fix: withJarb removed label type definition.
    There was a cast which says a `label` is a `string` this is incorrect it
    should use the type from the wrapper component.

--- src/form/withJarb/withJarb.tsx
+++ src/form/withJarb/withJarb.tsx
@@ -73,7 +73,7 @@ export default function withJarb<
         | 'valid'
         | 'error'
         | 'label'
-      > & { label?: string }
+      >
   ) {
     const illegalProps = managedProps.filter(p => props[p] !== undefined);
 
