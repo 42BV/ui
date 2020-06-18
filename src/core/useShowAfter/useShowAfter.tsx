ae473b839dd405ca8c94ba52c3b60729d6176832
Maarten Hus <maarten.hus@42.nl>
2020-06-18 11:31:57 +0200

    improvement: LoadingPage now accepts custom height and style.
    When the new height property is optional. When it is provided the height
    of the popover is no longer the fullscreen.
    
    Refactored the hook `LoadingPage` used to its own file, and called if
    `useShowAfter`. Also exposing the `useShowAfter` hook now so users of
    our library can use it as well.
    
    Closes #431

---
+++ src/core/useShowAfter/useShowAfter.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+/**
+ * Returns `false` but after a predetermined `after` time returns
+ * `true`.
+ *
+ * Useful for wanting to show content only after a time. For example
+ * to not show a loading spinner to soon, only
+ * after it takes a while.
+ *
+ * @param number after The amount of time in milliseconds after which the return value should become `true`.
+ */
+export function useShowAfter(after: number): boolean {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setShow(true);
+    }, after);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [after]);
+
+  return show;
+}
