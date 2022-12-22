0c3f746149e1f2f33c8bf477dc8bb4991c32fa7f
Gido Manders <gido.manders@42.nl>
2022-12-22 11:41:28 +0100

    improvement: add support for node 18 and 20
    Node 20 is now stable and the LTS version, so we should test if the
    library is compatible with that version as well.
    
    Added node 18 and 20 to GitHub workflow.
    Updated dependencies.

--- setupTests.ts
+++ setupTests.ts
@@ -1,44 +1,8 @@
 import { TextEncoder } from 'util';
+
 global.TextEncoder = TextEncoder;
 
 beforeEach(() => {
   jest.clearAllMocks();
   jest.resetAllMocks();
-
-  // Re-create the fake storages for each test the stores always start fresh.
-  setupStorages();
 });
-
-// Create fake versions of localStorage and sessionStorage.
-/* istanbul ignore next */
-function setupStorages() {
-  let localStorageMockStore = {};
-
-  // @ts-expect-error override for custom store in test
-  global.localStorage = {
-    getItem: jest.fn((key) => {
-      return localStorageMockStore[key] || null;
-    }),
-    setItem: jest.fn((key, value) => {
-      localStorageMockStore[key] = `${value}`;
-    }),
-    clear: jest.fn(() => {
-      localStorageMockStore = {};
-    })
-  };
-
-  let sessionStorageMockStore = {};
-
-  // @ts-expect-error override for custom store in test
-  global.sessionStorage = {
-    getItem: jest.fn((key) => {
-      return sessionStorageMockStore[key] || null;
-    }),
-    setItem: jest.fn((key, value) => {
-      sessionStorageMockStore[key] = `${value}`;
-    }),
-    clear: jest.fn(() => {
-      sessionStorageMockStore = {};
-    })
-  };
-}
