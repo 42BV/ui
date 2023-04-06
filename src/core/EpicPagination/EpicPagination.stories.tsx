375f646e4b85a4e12963fff1c2876454a370ed0d
Gido Manders <gido.manders@42.nl>
2023-04-06 11:49:13 +0200

    improvement: add page size dropdown to EpicPagination
    The Pagination component has a page size dropdown, but the
    EpicPagination does not provide that feature yet.
    
    Added optional page size dropdown to EpicPagination, similar to the one
    in Pagination.

--- src/core/EpicPagination/EpicPagination.stories.tsx
+++ src/core/EpicPagination/EpicPagination.stories.tsx
@@ -5,9 +5,22 @@ import { range } from 'lodash';
 import { pageOf } from '../../utilities/page/page';
 
 import { EpicPagination } from './EpicPagination';
+import { Alert } from 'reactstrap';
 
 storiesOf('core/EpicPagination', module)
   .addParameters({ component: EpicPagination })
+  .addDecorator((Story) => (
+    <>
+      <Alert color="warning" className="mb-4">
+        <p>
+          To be able to use EpicPagination, you have to add
+          @42.nl/spring-connect and lodash to your dependencies:
+        </p>
+        <code>npm install --save @42.nl/spring-connect lodash</code>
+      </Alert>
+      <Story />
+    </>
+  ))
   .add('default', () => {
     const [pageNumber, setPageNumber] = useState(5);
 
@@ -16,6 +29,60 @@ storiesOf('core/EpicPagination', module)
     return (
       <div className="d-flex justify-content-center">
         <EpicPagination page={page} onChange={setPageNumber} />
+      </div>
+    );
+  })
+  .add('without previous and next', () => {
+    const [pageNumber, setPageNumber] = useState(5);
+
+    const page = pageOf(range(1, 100), pageNumber, 10);
+
+    return (
+      <div className="d-flex justify-content-center">
+        <EpicPagination
+          page={page}
+          onChange={setPageNumber}
+          showPreviousAndNextButtons={false}
+        />
+      </div>
+    );
+  })
+  .add('with changeable page size', () => {
+    const [pageNumber, setPageNumber] = useState(5);
+    const [pageSize, setPageSize] = useState(10);
+
+    const page = pageOf(range(1, 200), pageNumber, pageSize);
+
+    return (
+      <div className="d-flex justify-content-center">
+        <EpicPagination
+          page={page}
+          onChange={setPageNumber}
+          onPageSizeChange={(size) => {
+            setPageSize(size);
+            setPageNumber(1);
+          }}
+        />
+      </div>
+    );
+  })
+  .add('with allowed page sizes', () => {
+    const [pageNumber, setPageNumber] = useState(5);
+    const [pageSize, setPageSize] = useState(8);
+
+    const page = pageOf(range(1, 200), pageNumber, pageSize);
+
+    return (
+      <div className="d-flex justify-content-center">
+        <EpicPagination
+          page={page}
+          onChange={setPageNumber}
+          onPageSizeChange={(size) => {
+            setPageSize(size);
+            setPageNumber(1);
+          }}
+          allowedPageSizes={[2, 4, 8, 16, 32]}
+        />
       </div>
     );
   });
